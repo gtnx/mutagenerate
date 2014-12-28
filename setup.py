3035fea48be7219191a0c0643045da61c49de758
Guillaume Thomas <guillaume.thomas642@gmail.com>
2014-12-28 14:48:33 +0100

    changed setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='mutagenerate',
-      version='0.1.0',
+      version='0.1.1',
       author='Guillaume Thomas',
       author_email='guillaume.thomas642@gmail.com',
       license='LICENCE.txt',
