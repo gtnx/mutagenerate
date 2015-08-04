6f4920be87703cc5645cb57f656d31a0cbdcfd50
Guillaume Thomas <guillaume.thomas642@gmail.com>
2015-08-04 10:34:39 +0200

    Added color in log

--- mutagenerate/log.py
+++ mutagenerate/log.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import sys
+from colorlog import ColoredFormatter
 
-logger = logging.getLogger("mutagenerate")
-logger.setLevel(logging.DEBUG)
+loggers = ('mutagenerate', 'amazon', 'musicbrainz')
+formatter = ColoredFormatter(
+    '%(log_color)s%(asctime)s.%(msecs)03d, %(name)s, %(levelname)s, %(message)s',
+    log_colors={
+        'DEBUG': 'green',
+        'INFO': 'blue',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'bold_purple',
+    },
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
-logger.addHandler(logging.NullHandler())
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(formatter)
+for logger in map(logging.getLogger, loggers):
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
