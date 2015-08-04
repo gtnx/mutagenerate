6485f3b21b073ac9bad9f8c0461459f726b90693
Guillaume Thomas <guillaume.thomas642@gmail.com>
2015-08-04 10:34:31 +0200

    Added util.py file

--- mutagenerate/util.py
+++ mutagenerate/util.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+# from __future__ import absolute_import, division, print_function
+
+import os
 import sys
 
 
@@ -38,3 +41,34 @@ def query_yes_no(question, default="yes"):
             return valid[choice]
         else:
             sys.stdout.write("Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
+
+
+def list_directory(directory, extension):
+    return [os.path.join(directory, fn) for fn in filter(lambda x: x.endswith('.' + extension), os.listdir(directory))]
+
+
+def rget(data, *keys, **kwargs):
+    try:
+        value = data[keys[0]]
+        for key in keys[1:]:
+            value = value[key]
+    except (KeyError, IndexError, TypeError):
+        return kwargs.get('default', None)
+    return value
+
+
+def ignore_errors(errors, logger):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except errors as e:
+                logger.error(e)
+        return wrapper
+    return decorator
+
+
+def format_rows_output(rows):
+    cols = zip(*rows)
+    col_widths = [max(len(unicode(value)) for value in col) for col in cols]
+    return '\n'.join([' | '.join([('%%%ds' % col_widths[i]) % unicode(value) for i, value in enumerate(row)]) for row in rows])
